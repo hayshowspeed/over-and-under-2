2fb09de16121f7c2e48a0825ee20b3c5a616ead6
hayshowspeed <124213731+hayshowspeed@users.noreply.github.com>
2023-02-01 18:40:07 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    jump = 1
+    master_car.vy = -200
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes`, function (sprite, location) {
+    game.over(false)
+    game.over(true)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes-up`, function (sprite, location) {
+    game.over(false)
+    music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    game.over(true)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`acid`, function (sprite, location) {
+    game.over(false)
+    game.over(true)
+    music.play(music.stringPlayable("G B A G C5 D A B ", 120), music.PlaybackMode.LoopingInBackground)
+})
+let jump = 0
+let master_car: Sprite = null
+tiles.setTilemap(tilemap`level1`)
+scene.setBackgroundImage(assets.image`background`)
+master_car = sprites.create(assets.image`truck3`, SpriteKind.Player)
+master_car.ay = 500
+master_car.vx = 100
+scene.cameraFollowSprite(master_car)
