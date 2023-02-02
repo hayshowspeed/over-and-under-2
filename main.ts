4f2526dc4f622a17c8a3935235160e43b6107aa8
hayshowspeed <124213731+hayshowspeed@users.noreply.github.com>
2023-02-01 20:36:04 -0400

    i added multiplayer in this update because i want to add multiplayer

--- main.ts
+++ main.ts
@@ -17,14 +17,19 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes-up`, function (sprite,
     music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
     game.over(true)
 })
+controller.player2.onEvent(ControllerEvent.Connected, function () {
+    _true = 0
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`acid`, function (sprite, location) {
     game.over(false)
     game.over(true)
     music.play(music.stringPlayable("G B A G C5 D A B ", 120), music.PlaybackMode.UntilDone)
 })
+let add_them = 0
+let projectile = false
+let _true = 0
 let stop = 0
 let left = 0
-let evil_eye: Sprite = null
 let master_car: Sprite = null
 info.changeScoreBy(0)
 info.changeLifeBy(8)
@@ -41,10 +46,12 @@ assets.animation`truck3 animated`,
 100,
 true
 )
-animation.runImageAnimation(
-evil_eye,
-assets.animation`enemy animated`,
-200,
-true
-)
-evil_eye = sprites.create(assets.image`enemy`, SpriteKind.Enemy)
+game.setGameOverMessage(false, "u lose")
+game.showLongText("there still bugs in joy mate", DialogLayout.Full)
+game.onUpdate(function () {
+    if (true) {
+        projectile = controller.player2.isPressed(ControllerButton.A)
+    } else {
+        add_them = controller.player2.dx()
+    }
+})
