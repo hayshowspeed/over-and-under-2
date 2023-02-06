f1452ca37fb1268110eecb77894735598fef39d9
hayshowspeed <124213731+hayshowspeed@users.noreply.github.com>
2023-02-06 14:38:18 -0400

    added hp but it bugs it do not help when am alone doing this that why the bugs

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    let stop = 0
-    left = stop
-    master_car.vy = -200
+    if (true) {
+        u_cant_hit_double_on_up_arrow = 1
+    } else {
+    	
+    }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`spikes`, function (sprite, location) {
     game.over(false)
@@ -25,12 +27,23 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`acid`, function (sprite, loca
 })
 let add_them = 0
 let projectile = false
-let left = 0
-let master_car: Sprite = null
+let u_cant_hit_double_on_up_arrow = 0
 let _true = 0
-let coin = 0
+let level = 0
+let when_u_get_too_the_end = 0
+let statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar.setLabel("HP")
+statusbar.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
+let when_u_get_to_the_end_of_a_level_u_go_too_the_next_one = 2
+when_u_get_to_the_end_of_a_level_u_go_too_the_next_one = 3
+when_u_get_to_the_end_of_a_level_u_go_too_the_next_one = 4
+when_u_get_too_the_end += 1
+let set_level = 1
+set_level = 2
+set_level = 3
+set_level = 4
 scene.setBackgroundColor(8)
-if (-30 > coin) {
+if (Math.round(7) > level) {
 	
 } else {
 	
@@ -42,7 +55,7 @@ console.log("100")
 console.logValue("-20", -40)
 info.changeScoreBy(0)
 info.changeLifeBy(8)
-coin = randint(0, 10)
+let coin = randint(0, 10)
 let map = 0
 tiles.setTilemap(tilemap`level0`)
 tiles.setTilemap(tilemap`level2`)
@@ -50,7 +63,7 @@ tiles.setTilemap(tilemap`level4`)
 tiles.setTilemap(tilemap`level5`)
 tiles.setTilemap(tilemap`level0`)
 scene.setBackgroundImage(assets.image`background`)
-master_car = sprites.create(assets.image`truck3`, SpriteKind.Player)
+let master_car = sprites.create(assets.image`truck3`, SpriteKind.Player)
 master_car.ay = 500
 master_car.vx = 100
 scene.cameraFollowSprite(master_car)
@@ -73,6 +86,7 @@ scroller.scrollBackgroundWithCamera(scroller.CameraScrollMode.BothDirections)
 scroller.scrollBackgroundWithSpeed(-70, -50)
 scroller.setCameraScrollingMultipliers(2, 4)
 controller.moveSprite(master_car, 100, 100)
+level = 10
 game.onUpdate(function () {
     if (true) {
         projectile = controller.player2.isPressed(ControllerButton.A)
